4813ade556f7c385797890e3701903ebd3eed40d
Nataliia D <113416916+SambykaLight@users.noreply.github.com>
2023-02-24 19:50:51 +0100

    with lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+galleryContainer.addEventListener('keydown', onEscKeyPress);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+// modal
+
+const instance = basicLightbox.create(
+  ` <img width="1280" height="auto" src="">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+  }
+);
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  const dataSource = evt.target.dataset.source;
+  if (!dataSource) {
+    return;
+  }
+  instance.element().querySelector("img").src = dataSource;
+  instance.show();
+}
+
+function onEscKeyPress(evt) {
+    if (evt.code === 'Escape') {
+        instance.close();
+
+  } 
+}
